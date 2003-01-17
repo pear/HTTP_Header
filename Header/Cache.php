564290c1618a9cddf2975c476d09052a50f43435
Wolfram Kriesing <cain@php.net>
2003-01-17 15:35:41 +0000

    - add methods to check cache times - add some comments
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_Header/trunk@112242 c90b9560-bf6c-de11-be94-00142212c4b1

--- Header/Cache.php
+++ Header/Cache.php
@@ -42,10 +42,39 @@ class HTTP_Header_Cache extends HTTP_Header
     {
         $this->_caching = $caching;
 
-        if( $this->_caching ) {
+        if ( $this->_caching ) {
             $this->setHeader('Pragma','cache');
             $this->setHeader('Cache-Control','public');
         }
+    }
+
+    function getCacheStart()
+    {
+        return $this->dateToTimestamp($_SERVER['HTTP_IF_MODIFIED_SINCE']);
+    }
+
+    /**
+    *   you can call it like this
+    *   isOlderThan( 2 , 'days' )
+    */
+    function isOlderThan( $time=0 , $unit='' )
+    {
+        switch (strtolower($unit)) {
+            case 'week':
+            case 'weeks':   $time = $time*7;
+            case 'day':
+            case 'days':    $time = $time*24;
+            case 'hour':
+            case 'hours':   $time = $time*60;
+            case 'minute':
+            case 'minutes': $time = $time*60;
+            case 'second':  break;
+        }
+
+        if ( $time && $this->getCacheStart()+$time < time() ) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -53,7 +82,7 @@ class HTTP_Header_Cache extends HTTP_Header
     *   which is simply checked for true
     *   it would be the same as
     *       $this->isCached() && $condition  ===  $this->isCached($condition)
-    *   it is suggested to use the parameter since the handling for sending the 
+    *   it is suggested to use the parameter since the handling for sending the
     *   'last-modified' is included in this method here
     *
     *   @author     Wolfram Kriesing <wolfram@kriesing.de>
@@ -61,7 +90,7 @@ class HTTP_Header_Cache extends HTTP_Header
     */
     function isCached( $condition=true )
     {
-        if( isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && $condition==true ) {
+        if ( isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && $condition==true ) {
             return true;
         }
 
@@ -69,7 +98,7 @@ class HTTP_Header_Cache extends HTTP_Header
         // then it shall be cached, and therefore we need to send
         // the 'last-modified' header in order to get a 'if-modified-since'
         // next time, so that we can answer with a 304 in case it is still cached
-        if( $this->_caching ) {
+        if ( $this->_caching ) {
             $this->setHeader( 'Last-Modified' );
         }
         return false;
@@ -77,10 +106,16 @@ class HTTP_Header_Cache extends HTTP_Header
 
 
 
-
-    function exitIfCached( $condition )
+    /**
+    *   this method exits the script if the page is
+    *   still cached by the user agent, the condition
+    *   if given will be AND-ed to the 'isCached' call
+    *
+    *   @param  boolean     will be AND-ed to the 'isCached' call
+    */
+    function exitIfCached( $condition=true )
     {
-        if( $this->isCached( $condition ) ) {
+        if ( $this->isCached( $condition ) ) {
             $this->sendHeaders();
             $this->sendStatusCode(304);
             exit;
