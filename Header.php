daf3f1ff41a82f7dba2c19026b8c9ff3e6e36aba
Michael Wallner <mike@php.net>
2004-12-09 12:32:00 +0000

    * typo
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_Header/trunk@174346 c90b9560-bf6c-de11-be94-00142212c4b1

--- Header.php
+++ Header.php
@@ -307,7 +307,7 @@ class HTTP_Header extends HTTP
      */
     function dateToTimestamp($date)
     {
-        static $monts = array(
+        static $months = array(
             null => 0, 'Jan' => 1, 'Feb' => 2, 'Mar' => 3, 'Apr' => 4,
             'May' => 5, 'Jun' => 6, 'Jul' => 7, 'Aug' => 8, 'Sep' => 9,
             'Oct' => 10, 'Nov' => 11, 'Dec' => 12
