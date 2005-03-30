d5fb6a73b1133c76bf9a7ecc7f814e8319201d72
Michael Wallner <mike@php.net>
2005-03-30 18:10:07 +0000

    * new docblock header
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_Header/trunk@183180 c90b9560-bf6c-de11-be94-00142212c4b1

--- Header.php
+++ Header.php
@@ -1,20 +1,31 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PEAR :: HTTP :: Header                                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is available at http://www.php.net/license/3_0.txt              |
-// | If you did not receive a copy of the PHP license and are unable      |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Wolfram Kriesing <wk@visionp.de>                            |
-// |          Davey Shafik <davey@php.net>                                |
-// |          Michael Wallner <mike@php.net>                              |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/**
+ * HTTP::Header
+ * 
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category    HTTP
+ * @package     HTTP_Header
+ * @author      Wolfram Kriesing <wk@visionp.de>
+ * @author      Davey Shafik <davey@php.net>
+ * @author      Michael Wallner <mike@php.net>
+ * @copyright   1997-2005 The PHP Group
+ * @license     http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version     CVS: $Id$
+ * @link        http://pear.php.net/package/HTTP_Download
+ */
+
+/**
+ * Requires HTTP
+ */
 require_once 'HTTP.php';
 
 /**#@+
